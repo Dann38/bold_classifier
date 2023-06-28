f5c9eebab69f98ca82cea69a01380f40efc25036
Даниил Копылов <it-daniil@yandex.ru>
2023-06-29 07:17:42 +0800

    change path data

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from dataset_reader.dataset_reader import Reader
+import os
+path_data = os.path.join(os.getcwd(), "dataset")
+geom_data = os.path.join(path_data, "Геометрия")
 
 reader = Reader()
-pages = reader.get_array_pages("C:\\Users\\danii\\program\\python\\project\\bold_classifier\\dataset\\Геометрия\\")
+pages = reader.get_array_pages(geom_data)
 
 print(pages)
 print(pages[0].image)
