48cabcf59348b3a705e2728ed411b68c75c7c19c
Даниил Копылов <it-daniil@yandex.ru>
2023-06-29 20:52:30 +0800

    rez without Nan

--- bold_classifier/clasterization_bold_classifier/mean_bold_classifier/mean_bold_classifier.py
+++ bold_classifier/clasterization_bold_classifier/mean_bold_classifier/mean_bold_classifier.py
@@ -1,5 +1,8 @@
+import cv2
+
 from ..clasterization_bold_classifier import ClasterizationBoldClassifier
 from binarizer.valley_emphasis_binarizer import ValleyEmphasisBinarizer  # TODO Изменить путь
+from binarizer.adap_binarizer import AdapBinarizer
 from ..utils import base_line_image, get_rid_spaces
 import numpy as np
 
@@ -9,7 +12,14 @@ class MeanBoldClassifier(ClasterizationBoldClassifier):
         ve_binarizer = ValleyEmphasisBinarizer()
         return ve_binarizer.binarize(image)
 
+        # adap = AdapBinarizer()                                                    # ДЛЯ НЕЧЕТКИХ ИЗОБРАЖЕНИЙ ПЛОХО
+        # gray = cv2.cvtColor(adap.binarize(image), cv2.COLOR_RGB2GRAY)
+        # ret, binary = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+        # return binary / 255
+
     def evaluation_method(self, image: np.ndarray) -> float:
         bl_image = base_line_image(image)
         image_s = get_rid_spaces(bl_image)
+        if np.isnan(image_s.mean()):
+            return 0.0
         return image_s.mean()
