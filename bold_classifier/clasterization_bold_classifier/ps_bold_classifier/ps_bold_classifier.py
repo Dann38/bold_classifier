ff1cd6c2109d9c29e1f639faa2d76e13dcbdaee7
Даниил Копылов <it-daniil@yandex.ru>
2023-06-29 20:50:58 +0800

    rez in [0, 1]

--- bold_classifier/clasterization_bold_classifier/ps_bold_classifier/ps_bold_classifier.py
+++ bold_classifier/clasterization_bold_classifier/ps_bold_classifier/ps_bold_classifier.py
@@ -1,3 +1,5 @@
+import cv2
+
 from ..clasterization_bold_classifier import ClasterizationBoldClassifier
 from binarizer.valley_emphasis_binarizer import ValleyEmphasisBinarizer  # TODO Изменить путь
 from ..utils import base_line_image, get_rid_spaces
@@ -14,7 +16,12 @@ class PsBoldClassifier(ClasterizationBoldClassifier):
         image_s = get_rid_spaces(image_p)
         hw = image_s.shape[0] * image_s.shape[1]
         p_img = image_p[:, :-1] - image_p[:, 1:]
-        p_img[p_img > 0] = 1
+        p_img[p_img > 0] = 1.
+        p_img[p_img < 0] = 0.
         p = p_img.sum()
         s = hw - image_s.sum()
+        if p > s:
+            return 1.
+        if s == 0:
+            return 1.
         return p / s
