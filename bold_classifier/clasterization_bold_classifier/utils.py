f38998b3d376cf6929251b90c1354cf2c1f67388
Даниил Копылов <it-daniil@yandex.ru>
2023-06-29 17:21:22 +0800

    add function vector2listlist, listlist2vector for clasterization

--- bold_classifier/clasterization_bold_classifier/utils.py
+++ bold_classifier/clasterization_bold_classifier/utils.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from typing import List
 PERMISSIBLE_H_BBOX = 5
 
 
@@ -34,3 +34,24 @@ def base_line_image(image: np.ndarray) -> np.ndarray:
 
     return image[h_min:h_max + 1, :]
 
+
+def listlist2vector(listlist: List[List[float]], len_list: List[int]) -> np.ndarray:
+    N = sum(len_list)
+    vector = np.zeros((N,))
+    index = 0
+    for line in listlist:
+        for i in range(len(line)):
+            vector[index] = line[i]
+            index += 1
+    return vector
+
+
+def vector2listlist(vector: np.ndarray, len_list: List[int]) -> List[List[float]]:
+    listlist = []
+    index = 0
+    for i in range(len(len_list)):
+        listlist.append([])
+        for j in range(len_list[i]):
+            listlist[i].append(vector[index])
+            index += 1
+    return listlist
