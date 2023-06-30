c293b094b64663b60ae8c93450f945c61562be05
Даниил Копылов <it-daniil@yandex.ru>
2023-06-30 17:49:15 +0800

    calling clustering via a class

--- bold_classifier/clasterization_bold_classifier/clasterization_bold_classifier.py
+++ bold_classifier/clasterization_bold_classifier/clasterization_bold_classifier.py
@@ -1,13 +1,16 @@
 from abc import abstractmethod
 from ..bold_classifier import BaseBoldClassifier, BOLD, REGULAR
-from .utils import listlist2vector, vector2listlist
-from typing import List
+from clusterizater import Bold2MeanClusterizater
+from typing import List, Dict
 import numpy as np
 from dataset_reader.bbox import BBox  # TODO Изменить путь
-from sklearn.cluster import KMeans
+from dataset_reader.page import Page
 
 
 class ClasterizationBoldClassifier(BaseBoldClassifier):
+    def __init__(self, clusterizater=Bold2MeanClusterizater):
+        self.clusterizater = clusterizater()
+
     def classify(self, image: np.ndarray,  bboxes: List[List[BBox]]) -> List[List[float]]:
         processed_image = self.preprocessing(image)
         lines_estimates = self.get_evaluation_bboxes(processed_image, bboxes)
@@ -25,7 +28,7 @@ class ClasterizationBoldClassifier(BaseBoldClassifier):
             evaluation_bboxes.append([])
             for bbox in line:
                 image_bbox = image[bbox.y_top_left:bbox.y_bottom_right,
-                             bbox.x_top_left:bbox.x_bottom_right]
+                                   bbox.x_top_left:bbox.x_bottom_right]
                 evaluation_bbox = self.evaluation_method(image_bbox)
                 evaluation_bboxes[-1].append(evaluation_bbox)
         return evaluation_bboxes
@@ -36,29 +39,10 @@ class ClasterizationBoldClassifier(BaseBoldClassifier):
 
     def clasterization(self, lines_estimates: List[List[float]]) -> List[List[float]]:
         len_lines = [len(line) for line in lines_estimates]
-        X = listlist2vector(lines_estimates, len_lines)
-        XX = X.copy()
-        XX[:-1] += X[1:]
-        XX[1:] += X[:-1]
-        XX[0] += X[0]
-        XX[-1] += X[-1]
-        XX = XX/3.
-        X_vec = [[X[i], XX[i]] for i in range(len(X))]
-        kmeans = KMeans(n_clusters=2, n_init="auto")
-        kmeans.fit(X_vec)
-        X_clust = kmeans.labels_
-
-        cluster0 = kmeans.cluster_centers_[0][0]
-        cluster1 = kmeans.cluster_centers_[1][0]
-
-        bold_cluster = min(cluster0, cluster1)
-        regular_cluster = max(cluster0, cluster1)
-        if cluster0 == bold_cluster:
-            X_clust = 1.0 - X_clust
-
-        lines_bold_indicators = vector2listlist(X_clust, len_lines)
-        return lines_bold_indicators
-
+        X = self.clusterizater.listlist2vector(lines_estimates, len_lines)
+        X_cluster = self.clusterizater.clusterization(X)
+        lines_estimates = self.clusterizater.vector2listlist(X_cluster, len_lines)
+        return lines_estimates
 
     # Возращение результатов без кластеризации (полезна при отладке)
     def get_lines_estimates(self, image: np.ndarray,  bboxes: List[List[BBox]]) -> List[List[float]]:
@@ -66,8 +50,28 @@ class ClasterizationBoldClassifier(BaseBoldClassifier):
         lines_estimates = self.get_evaluation_bboxes(processed_image, bboxes)
         return lines_estimates
 
+    def evalusion_on_dataset(self, pages: List[Page]) -> Dict:
+        count_pages = len(pages)
+        evalution_sum = {
+            "precession": 0,
+            "recall": 0,
+            "F1": 0,
+            "accuracy": 0,
+            "N": 0
+        }
 
+        for num_page in range(count_pages):
+            listlist = self.classify(pages[num_page].image, pages[num_page].bboxes)
+            evalution_method = self.clusterizater.evalution_listlist(listlist, pages[num_page].style)
+            print(evalution_method)
+            for key in evalution_sum.keys():
+                if key == "N":
+                    evalution_sum[key] += evalution_method["N"]
+                else:
+                    evalution_sum[key] += evalution_method["N"] * evalution_method[key]
 
+        for key in evalution_sum.keys():
+            if key != "N":
+                evalution_sum[key] = evalution_sum[key] / evalution_sum["N"]
 
-
-
+        return evalution_sum
