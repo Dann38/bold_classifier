2ba71dc7a3ddea0f8930d3d59290f2196a8304a4
Даниил Копылов <it-daniil@yandex.ru>
2023-06-30 22:01:40 +0800

    it is better to highlight the bold with the regular than the regular bold

--- clusterizater/kmean_clusterizater/bold_2mean_clusterizater.py
+++ clusterizater/kmean_clusterizater/bold_2mean_clusterizater.py
@@ -16,13 +16,15 @@ class Bold2MeanClusterizater(BaseClusterizater):
         X_vec = [[X[i], XX[i]] for i in range(len(X))]
         kmeans = KMeans(n_clusters=2, n_init="auto")
         kmeans.fit(X_vec)
-        X_clust = kmeans.labels_
+        # X_clust = kmeans.labels_
 
         cluster0 = kmeans.cluster_centers_[0][0]
         cluster1 = kmeans.cluster_centers_[1][0]
 
         bold_cluster = min(cluster0, cluster1)
         regular_cluster = max(cluster0, cluster1)
-        if cluster0 == bold_cluster:
-            X_clust = 1.0 - X_clust
+        print(bold_cluster,  regular_cluster)
+        distance_cluster = regular_cluster-bold_cluster
+        X_clust = np.zeros_like(X)
+        X_clust[X-bold_cluster < bold_cluster+distance_cluster*0.05] = 1.
         return X_clust
