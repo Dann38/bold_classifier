f32a41443953273fa065672ffa8440187a3c25a1
Даниил Копылов <it-daniil@yandex.ru>
2023-07-03 14:16:26 +0800

    testing methods on 3 dataset

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 from dataset_reader.dataset_reader import Reader
-from bold_classifier import PsBoldClassifier, MeanBoldClassifier
+from bold_classifier import PsBoldClassifier, MeanBoldClassifier, HistBoldClassifier
 import os
 from dataset_reader.drawer import Drawer
-from clusterizater import BoldSpectralClusterizater
 
-path_data = os.path.join(os.getcwd(), "dataset")
-name_data = os.path.join(path_data, "ВКР")
+list_name_data_set = ["ВКР", "ГОСТ", "Геометрия"]
+classifier_list = [PsBoldClassifier, MeanBoldClassifier, HistBoldClassifier]
 
 reader = Reader()
-pages = reader.get_array_pages(name_data)
 
-ps_classifier = PsBoldClassifier(clusterizater=BoldSpectralClusterizater)
-mean_classifier = MeanBoldClassifier(clusterizater=BoldSpectralClusterizater)
+for name_data_set in list_name_data_set:
+    path_data = os.path.join(os.getcwd(), "dataset")
+    name_data = os.path.join(path_data, name_data_set)
+    pages = reader.get_array_pages(name_data)
 
-# print(ps_classifier.evalusion_on_dataset(pages))
+    print("*"*15, name_data_set, "*"*15)
+    for classifier in classifier_list:
+        cl = classifier()
+        rez = cl.evalusion_on_dataset(pages)
+        print("-"*10, classifier.__name__, "-"*10)
+        for kei in rez.keys():
+            print(f"{kei+':':{12}} {rez[kei]:.2f}")
 
-drawer = Drawer()
-num_page = 7
-rez_ps = ps_classifier.classify(pages[num_page].image, pages[num_page].bboxes)
-print(ps_classifier.clusterizater.evalution_listlist(rez_ps, pages[num_page].style))
-drawer.imshow(pages[num_page].image, pages[num_page].bboxes, rez_ps)
+# drawer = Drawer()
+# num_page = 7
+# classifier = classifier_list[0]
+# rez = classifier.classify(pages[num_page].image, pages[num_page].bboxes)
+# print(classifier.clusterizater.evalution_listlist(rez, pages[num_page].style))
+# drawer.imshow(pages[num_page].image, pages[num_page].bboxes, rez)
