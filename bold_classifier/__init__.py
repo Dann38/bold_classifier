01c2f04c8894db46c95e6c65070a37f0e66d81b5
Даниил Копылов <it-daniil@yandex.ru>
2023-07-03 14:14:24 +0800

    add HistBoldClassifier

--- bold_classifier/__init__.py
+++ bold_classifier/__init__.py
@@ -1,1 +1,1 @@
-from .clasterization_bold_classifier import PsBoldClassifier, MeanBoldClassifier
+from .clasterization_bold_classifier import PsBoldClassifier, MeanBoldClassifier, HistBoldClassifier
