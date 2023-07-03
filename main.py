27b874a1627d58f1f079154aa6e9aa50d6b168e2
Dann38 <38dunn@gmail.com>
2023-07-03 17:53:20 +0800

    new test in main

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import os
 from dataset_reader.drawer import Drawer
 
 list_name_data_set = ["ВКР", "ГОСТ", "Геометрия"]
-classifier_list = [PsBoldClassifier, MeanBoldClassifier, HistBoldClassifier]
+classifier_list = [PsBoldClassifier] #  MeanBoldClassifier, HistBoldClassifierS
 
 reader = Reader()
 
+
+# ТЕСТ =============================================================
 for name_data_set in list_name_data_set:
     path_data = os.path.join(os.getcwd(), "dataset")
     name_data = os.path.join(path_data, name_data_set)
@@ -21,9 +23,28 @@ for name_data_set in list_name_data_set:
         for kei in rez.keys():
             print(f"{kei+':':{12}} {rez[kei]:.2f}")
 
+
+# # ПОСМОТЕРТЬ РЕЗУЛЬТАТ ОДНОГО ===============================================
+# path_data = os.path.join(os.getcwd(), "dataset")
+# name_data = os.path.join(path_data, list_name_data_set[1])
+# pages = reader.get_array_pages(name_data)
 # drawer = Drawer()
 # num_page = 7
-# classifier = classifier_list[0]
+# classifier = classifier_list[0]()
 # rez = classifier.classify(pages[num_page].image, pages[num_page].bboxes)
 # print(classifier.clusterizater.evalution_listlist(rez, pages[num_page].style))
 # drawer.imshow(pages[num_page].image, pages[num_page].bboxes, rez)
+
+# СОХРАНИТЬ РЕЗУЛЬТАТ РАБОТЫ
+# os.mkdir("rez")
+#
+# path_data = os.path.join(os.getcwd(), "dataset")
+# name_data = os.path.join(path_data, list_name_data_set[2])
+# pages = reader.get_array_pages(name_data)
+# drawer = Drawer()
+# for i in range(len(pages)):
+#     num_page = i
+#     classifier = classifier_list[0]()
+#     rez = classifier.classify(pages[num_page].image, pages[num_page].bboxes)
+#     print(classifier.clusterizater.evalution_listlist(rez, pages[num_page].style))
+#     drawer.imsave(pages[num_page].image, pages[num_page].bboxes, rez, os.path.join("rez", f"{i}.jpeg"))
