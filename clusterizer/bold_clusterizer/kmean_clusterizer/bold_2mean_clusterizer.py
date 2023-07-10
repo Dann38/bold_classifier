947beffde9af7f34d52a109cd5f59acb8778699d
Даниил Копылов <it-daniil@yandex.ru>
2023-07-10 19:14:59 +0800

    fix n_init="auto" -> =10

--- clusterizer/bold_clusterizer/kmean_clusterizer/bold_2mean_clusterizer.py
+++ clusterizer/bold_clusterizer/kmean_clusterizer/bold_2mean_clusterizer.py
@@ -7,7 +7,7 @@ from ..bold_clusterizer import BaseBoldClusterizer
 
 class Bold2MeanClusterizer(BaseBoldClusterizer):
     def _get_clusters(self, x_vectors: np.ndarray) -> np.ndarray:
-        kmeans = KMeans(n_clusters=2, n_init="auto")
+        kmeans = KMeans(n_clusters=2, n_init=10)
         kmeans.fit(x_vectors)
         x_clusters = kmeans.labels_
         return x_clusters
