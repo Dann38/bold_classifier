f54001152bebf6373409ea41ae4217cad3160261
Даниил Копылов <it-daniil@yandex.ru>
2023-07-12 22:22:30 +0800

    fix norm

--- bold_classifier/clusterization_bold_classifier/ps_bold_classifier/ps_bold_classifier.py
+++ bold_classifier/clusterization_bold_classifier/ps_bold_classifier/ps_bold_classifier.py
@@ -7,14 +7,13 @@ class PsBoldClassifier(ClusterizationBoldClassifier):
     def evaluation_one_bbox_image(self, image: np.ndarray) -> float:
         base_line_image = self._get_base_line_image(image)  # baseline - main font area
         base_line_image_without_sparces = self._get_rid_spaces(base_line_image)  # removing spaces from a string
-        h, w = base_line_image_without_sparces.shape[:2]
 
         p_img = base_line_image[:, :-1] - base_line_image[:, 1:]
         p_img[abs(p_img) > 0] = 1.
         p_img[p_img < 0] = 0.
-        p = p_img.sum()
+        p = p_img.mean()
 
-        s = h * w - base_line_image_without_sparces.sum()
+        s = 1 - base_line_image_without_sparces.mean()
 
         if p > s or s == 0:
             evaluation = 1.
